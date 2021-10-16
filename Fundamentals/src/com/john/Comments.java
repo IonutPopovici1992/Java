d13c5c93e77e79022efbf3833b487db5804f0772
John <ionut_popovici92@yahoo.com>
2021-10-17 00:47:07 +0300

    Comments

---
+++ Fundamentals/src/com/john/Comments.java
@@ -0,0 +1,14 @@
+package com.john;
+
+public class Comments {
+    // Single-line Comments
+    /*
+    * Multi-line Comments
+    * The code below will print the word "Comments" to the screen,
+    * and it is amazing
+    */
+    public static void main(String[] args) {
+        System.out.println("Comments");
+    }
+
+}

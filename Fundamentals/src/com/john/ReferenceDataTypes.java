ed062598ed7b30b9fab120846cd534e36a3ca352
John <ionut_popovici92@yahoo.com>
2021-11-28 20:34:51 +0200

    Reference Data Types

--- Fundamentals/src/com/john/ReferenceDataTypes.java
+++ Fundamentals/src/com/john/ReferenceDataTypes.java
@@ -1,6 +1,24 @@
 package com.john;
 
+import java.time.LocalDate;
+
 /// Reference Data Types
 public class ReferenceDataTypes {
+
+    public static void main(String[] args) {
+        // Non primitive data types AKA reference data types
+        String name = new String("Amigoscode");
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        System.out.println();
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+        System.out.println(now.getYear());
+        System.out.println(now.getMonth());
+        System.out.println(now.getDayOfMonth());
+    }
 
 }

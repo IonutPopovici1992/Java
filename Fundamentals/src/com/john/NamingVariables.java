cbeef9b9ab2dcce9b67993c154bbda84b9f6ec8c
John <ionut_popovici92@yahoo.com>
2021-12-01 12:25:15 +0200

    Naming Variables

---
+++ Fundamentals/src/com/john/NamingVariables.java
@@ -0,0 +1,21 @@
+package com.john;
+
+/// Naming Variables
+public class NamingVariables {
+
+    public static void main(String[] args) {
+        // int main = 0; *** Not Allowed
+        int zero = 0;
+        int one = 1;
+
+        double pi = 3.1415;
+
+        int subscriberCountForAmigoscode = 1_000_000;
+
+        System.out.println(zero);
+        System.out.println(one);
+        System.out.println(pi);
+        System.out.println(subscriberCountForAmigoscode);
+    }
+
+}

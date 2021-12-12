11b20816fed990a62a86558afc90aa021835a854
John <ionut_popovici92@yahoo.com>
2021-12-12 23:40:17 +0200

    Arithmetic Operations

---
+++ Fundamentals/src/com/john/ArithmeticOperations.java
@@ -0,0 +1,17 @@
+package com.john;
+
+/// Arithmetic Operations
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        // Arithmetic Operations
+        System.out.println(10 + 2);
+        System.out.println(10 - 2);
+        System.out.println(10 * 2);
+        System.out.println(10 % 2);
+        System.out.println(10 / 2);
+        // BODMAS
+        System.out.println(10 * (2 + 7) - 10);
+    }
+
+}

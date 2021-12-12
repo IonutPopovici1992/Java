6311fef0edf168fbd176cfc4a979ebfb4c95e039
John <ionut_popovici92@yahoo.com>
2021-12-12 22:22:09 +0200

    The String Class

--- Fundamentals/src/com/john/TheStringClass.java
+++ Fundamentals/src/com/john/TheStringClass.java
@@ -3,4 +3,20 @@ package com.john;
 /// The String Class
 public class TheStringClass {
 
+    public static void main(String[] args) {
+        // The String Class
+        String name = "Amigoscode";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(1));
+
+        String code1 = "code";
+        System.out.println(name.contains(code1));
+        System.out.println(name.equals(code1));
+
+        String code2 = "codes";
+        System.out.println(name.contains(code2));
+        System.out.println(name.equals(code2));
+    }
+
 }
--- Fundamentals/src/com/john/Variables.java
+++ Fundamentals/src/com/john/Variables.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 /// Variables
 public class Variables {
+
     public static void main(String[] args) {
         int number = 100;
         double number2 = 100.01;
         String brand = "Amigoscode";
         Date date = new Date();
     }
+
 }

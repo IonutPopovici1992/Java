27ac16993e649d5dffe27d2e326f97703b5733ac
John <ionut_popovici92@yahoo.com>
2021-12-13 12:51:09 +0200

    The Math Class

---
+++ Fundamentals/src/com/john/MathClass.java
@@ -0,0 +1,16 @@
+package com.john;
+
+/// The Math Class
+public class MathClass {
+
+    public static void main(String[] args) {
+        // Math
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.max(5, 10));
+        System.out.println((int) Math.pow(9, 2));
+        System.out.println((int) Math.sqrt(81));
+        System.out.println(Math.PI);
+    }
+
+}

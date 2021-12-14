922354d14dac46593f380e5cc3d26b88a265a7de
John <ionut_popovici92@yahoo.com>
2021-12-14 12:43:40 +0200

    Comparison Operators

---
+++ Fundamentals/src/com/john/ComparisonOperators.java
@@ -0,0 +1,23 @@
+package com.john;
+
+/// Comparison Operators
+public class ComparisonOperators {
+
+    public static void main(String[] args) {
+        // Comparison Operators
+        int khalidAge = 18;
+        int mariamAge = 20;
+
+        boolean isKhalidOlderThanMariam = khalidAge > mariamAge;
+        System.out.println(isKhalidOlderThanMariam);
+        System.out.println();
+
+        System.out.println(khalidAge < mariamAge);
+        System.out.println(khalidAge <= mariamAge);
+        System.out.println(khalidAge > mariamAge);
+        System.out.println(khalidAge >= mariamAge);
+        System.out.println(khalidAge == mariamAge);
+        System.out.println(khalidAge != mariamAge);
+    }
+
+}

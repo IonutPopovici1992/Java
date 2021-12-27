2f7048574a5ec913817dd8b5b42bb45e7a14f13d
John <ionut_popovici92@yahoo.com>
2021-12-27 22:55:36 +0200

    Logical Operators

--- Fundamentals/src/com/john/LogicalOperators.java
+++ Fundamentals/src/com/john/LogicalOperators.java
@@ -4,6 +4,17 @@ package com.john;
 public class LogicalOperators {
 
     public static void main(String[] args) {
+        // Logical Operators
+        boolean isAdult = false;
+        boolean isStudent = true;
+        boolean isAmigoscodeMember = true;
 
+        // Logical "and" - &&
+        System.out.println(isAdult && isStudent);
+        // Logical "or" - ||
+        System.out.println(isAdult || isStudent);
+        // Logical "not" - !
+        System.out.println((!isAdult || isStudent) && !isAmigoscodeMember);
     }
+
 }

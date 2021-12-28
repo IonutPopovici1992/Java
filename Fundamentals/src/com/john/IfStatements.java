bf22b45dd21dd49350f2030054ce165f1a2c323a
John <ionut_popovici92@yahoo.com>
2021-12-28 11:45:33 +0200

    If Statements

---
+++ Fundamentals/src/com/john/IfStatements.java
@@ -0,0 +1,18 @@
+package com.john;
+
+/// If Statements
+public class IfStatements {
+
+    public static void main(String[] args) {
+        // If Statements
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Hooray... I am an adult.");
+        } else if (age >= 16 && age <= 18) {
+            System.out.println("I am almost an adult.");
+        } else {
+            System.out.println("I am not an adult.");
+        }
+    }
+
+}

503032692a150597e4e91b9f5bf0034db695af42
John <ionut_popovici92@yahoo.com>
2021-12-29 12:23:06 +0200

    Ternary Operator

---
+++ Fundamentals/src/com/john/TernaryOperator.java
@@ -0,0 +1,17 @@
+package com.john;
+
+/// Ternary Operator
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        // Ternary Operator
+        int age = 18;
+
+        String message = age >= 18 ?
+                "Hooray... I am an adult. :)" :
+                "I am not an adult. :(";
+
+        System.out.println(message);
+    }
+
+}

4f8d13357fd490bbaf69a87c727bc881a260143c
John <ionut_popovici92@yahoo.com>
2022-01-04 13:03:20 +0200

    Switch Statements

--- Fundamentals/src/com/john/SwitchStatements.java
+++ Fundamentals/src/com/john/SwitchStatements.java
@@ -5,6 +5,31 @@ public class SwitchStatements {
 
     public static void main(String[] args) {
         // Switch Statements
+        String gender = "FEMALE";
+
+        if (gender.equals("FEMALE")) {
+            // ...
+        } else if (gender.equals("MALE")) {
+            // ...
+        } else if (gender.equals("PREFER_NOT_SAY")) {
+            // ...
+        } else {
+            // ...
+        }
+
+        switch (gender.toUpperCase()) {
+            case "FEMALE":
+                System.out.println("I am a female.");
+                break;
+            case "MALE":
+                System.out.println("I am a male.");
+                break;
+            case "PREFER_NOT_SAY":
+                System.out.println("Prefer not say.");
+                break;
+            default:
+                System.out.println("Unknown gender!");
+        }
     }
 
 }

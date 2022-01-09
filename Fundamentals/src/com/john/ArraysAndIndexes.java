b97e22db73fc6a0cf8eaed54a08a4709782c6d06
John <ionut_popovici92@yahoo.com>
2022-01-09 22:11:45 +0200

    Arrays and Indexes

--- Fundamentals/src/com/john/ArraysAndIndexes.java
+++ Fundamentals/src/com/john/ArraysAndIndexes.java
@@ -5,6 +5,12 @@ public class ArraysAndIndexes {
 
     public static void main(String[] args) {
         // Arrays and Indexes
+        int [] numbers = {100, 200, 300, 400, 500, 600, 700, 800};
+        int firstNumber = numbers[0];
+        int lastNumber = numbers[numbers.length - 1];
+
+        System.out.println(firstNumber);
+        System.out.println(lastNumber);
     }
 
 }

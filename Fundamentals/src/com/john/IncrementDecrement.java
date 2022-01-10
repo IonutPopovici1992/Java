4819d805466af5af000ff76ec5930190c5821b21
John <ionut_popovici92@yahoo.com>
2022-01-10 12:04:12 +0200

    ++ & --

---
+++ Fundamentals/src/com/john/IncrementDecrement.java
@@ -0,0 +1,26 @@
+package com.john;
+
+/// Increment and Decrement
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+        // Increment and Decrement
+        int number = 10;
+        // number = number + 1;
+        number++;
+        System.out.println(number);
+
+        // number = number - 1;
+        number--;
+        System.out.println(number);
+
+        // number = number + 2;
+        number += 2;
+        System.out.println(number);
+
+        // number = number - 2;
+        number -= 2;
+        System.out.println(number);
+    }
+
+}

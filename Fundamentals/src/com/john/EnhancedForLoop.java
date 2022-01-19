2c4cc2932c2214f61cc58d50956f6f75d6ce0f47
John <ionut_popovici92@yahoo.com>
2022-01-19 23:49:49 +0200

    Enhanced For Loop

--- Fundamentals/src/com/john/EnhancedForLoop.java
+++ Fundamentals/src/com/john/EnhancedForLoop.java
@@ -4,8 +4,28 @@ package com.john;
 public class EnhancedForLoop {
 
     public static void main(String[] args) {
-        // Enhanced For Loop
+        // Loops
+        int[] numbers = {10, 20, 30, 40, 50};
+        String[] names = {"Anna", "Ali"};
 
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println();
+
+        // Enhanced For Loop
+        System.out.println("Enhanced For Loop");
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        System.out.println();
+
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 
 }

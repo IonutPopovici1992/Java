dc75bb2bf8125e9c29d134e13d4246ea1ba6e40e
John <ionut_popovici92@yahoo.com>
2022-02-23 02:22:37 +0200

    Do While

---
+++ Fundamentals/src/com/john/DoWhile.java
@@ -0,0 +1,16 @@
+package com.john;
+
+/// Do While
+public class DoWhile {
+
+    public static void main(String[] args) {
+        // Do While
+        int count = 21;
+
+        do {
+            System.out.println("Count " + count);
+            count++;
+        } while (count <= 20);
+    }
+
+}

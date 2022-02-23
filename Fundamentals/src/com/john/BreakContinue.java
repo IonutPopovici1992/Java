5ce60d82f5b525a4d1f82e0946678523bba38a8b
John <ionut_popovici92@yahoo.com>
2022-02-23 02:02:46 +0200

    While Loop

--- Fundamentals/src/com/john/BreakContinue.java
+++ Fundamentals/src/com/john/BreakContinue.java
@@ -1,4 +1,37 @@
 package com.john;
 
+/// Break and Continue
 public class BreakContinue {
+
+    public static void main(String[] args) {
+        // Break and Continue
+        String[] names = {"Anna", "Ali", "Bob", "Mike"};
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        // break
+        for (String name : names) {
+            if (name.equals("Bob")) {
+                break;
+            }
+
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        // continue
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                continue;
+            }
+
+            System.out.println(name);
+        }
+    }
+
 }
---
+++ Fundamentals/src/com/john/WhileLoop.java
@@ -0,0 +1,16 @@
+package com.john;
+
+/// While Loop
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        // While Loop
+        int count = 0;
+
+        while (count <= 20) {
+            System.out.println("Count " + count);
+            count++;
+        }
+    }
+
+}

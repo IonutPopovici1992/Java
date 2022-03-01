6d3abc6b24b1f8b324a855031be67edf564adb49
John <ionut_popovici92@yahoo.com>
2022-03-01 17:09:04 +0200

    Understanding Methods

---
+++ Fundamentals/src/com/john/Methods2.java
@@ -0,0 +1,21 @@
+package com.john;
+
+/// Understanding Methods
+public class Methods2 {
+
+    public static void main(String[] args) {
+        // Understanding Methods
+
+        char[] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D'};
+        int count = countOccurrences(letters);
+
+        System.out.println(count);
+    }
+
+    public static int countOccurrences(char [] letters) {
+        System.out.println("Method countOccurrences() was invoked!");
+
+        return 0;
+    }
+
+}

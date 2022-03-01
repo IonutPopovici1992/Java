6f5f92c8ef2331de7ad3ea307f0b9edc576865b8
John <ionut_popovici92@yahoo.com>
2022-03-01 14:25:40 +0200

    Taking User Input

--- Fundamentals/src/com/john/ScannerClass.java
+++ Fundamentals/src/com/john/ScannerClass.java
@@ -1,3 +1,31 @@
 package com.john;
 
-public class ScannerClass {}
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/// Scanner Class
+public class ScannerClass {
+
+    public static void main(String[] args) {
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println();
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        int year = LocalDate.now().minusYears(age).getYear();
+        System.out.println("You were born in " + year + ", ");
+
+        if (age < 18) {
+            System.out.println("and you are not an adult! :(");
+        } else {
+            System.out.println("and you are an adult! :)");
+        }
+    }
+
+}

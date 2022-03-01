ad32daaa02cdffe38deb118847eef730fa0f69ce
John <ionut_popovici92@yahoo.com>
2022-03-01 15:59:30 +0200

    Methods

---
+++ Fundamentals/src/com/john/Methods.java
@@ -0,0 +1,20 @@
+package com.john;
+
+/// Methods
+public class Methods {
+
+    public static void main(String[] args) {
+        // Methods
+        System.out.println("Hello, World!");
+
+        System.out.println();
+
+        String brand = "Samsung";
+        System.out.println(brand);
+        System.out.println(brand.toUpperCase());
+        System.out.println(brand.startsWith("S"));
+        System.out.println(brand.contains("Z"));
+        System.out.println(brand.endsWith("g"));
+    }
+
+}

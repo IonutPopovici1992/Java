842d843f9e969e26a42f9f5ba255b2839aa3c1ee
John <ionut_popovici92@yahoo.com>
2022-03-02 22:56:00 +0200

    User Defined Methods

--- Fundamentals/src/com/john/Methods2.java
+++ Fundamentals/src/com/john/Methods2.java
@@ -1,21 +1,27 @@
 package com.john;
 
 /// Understanding Methods
+/// User Defined Methods
 public class Methods2 {
 
     public static void main(String[] args) {
         // Understanding Methods
 
         char[] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D'};
-        int count = countOccurrences(letters);
+        int count = countOccurrences(letters, 'Z');
 
         System.out.println(count);
     }
 
-    public static int countOccurrences(char [] letters) {
-        System.out.println("Method countOccurrences() was invoked!");
-
-        return 0;
+    public static int countOccurrences(char [] letters, char searchLetter) {
+        // System.out.println("Method countOccurrences() was invoked!");
+        int count = 0;
+        for (char letter : letters) {
+            if (letter == searchLetter) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }

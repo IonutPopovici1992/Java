d5d691f9a9540532ee1a4bc4684d2722201f076a
John <ionut_popovici92@yahoo.com>
2022-03-03 21:46:21 +0200

    Classes and Objects

---
+++ Fundamentals/src/com/john/ClassesObjects.java
@@ -0,0 +1,44 @@
+package com.john;
+
+/// Classes and Objects
+public class ClassesObjects {
+
+    public static void main(String[] args) {
+        // Classes and Objects
+        Lens lensOne = new Lens("sony", "85mm", true);
+        Lens lensTwo = new Lens("sony", "30mm", true);
+        Lens lensThree = new Lens("canon", "24-70mm", false);
+
+        System.out.println("Lens 1");
+        System.out.println(lensOne.brand);
+        System.out.println(lensOne.focalLength);
+        System.out.println(lensOne.isPrime);
+
+        System.out.println();
+
+        System.out.println("Lens 2");
+        System.out.println(lensTwo.brand);
+        System.out.println(lensTwo.focalLength);
+        System.out.println(lensTwo.isPrime);
+
+        System.out.println();
+
+        System.out.println("Lens 3");
+        System.out.println(lensThree.brand);
+        System.out.println(lensThree.focalLength);
+        System.out.println(lensThree.isPrime);
+    }
+
+    static class Lens {
+        String brand;
+        String focalLength;
+        boolean isPrime;
+
+        Lens(String brand, String focalLength, boolean isPrime) {
+            this.brand = brand;
+            this.focalLength = focalLength;
+            this.isPrime = isPrime;
+        }
+    }
+
+}
